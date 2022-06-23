115e2c4af85e9652815bc526997a69d31b8bc767
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-23 19:54:02 +0530

    array program unique element

--- Array/code1/index.js
+++ Array/code1/index.js
@@ -8,11 +8,21 @@ let arr = [
   "mayur",
 ];
 
+//Way 1
 console.log(arr);
 let unique = arr.filter((item, index) => {
   return arr.indexOf(item) === index;
 });
 console.log(unique);
+
+//Way 2
+function UniqueElements(arr) {
+  const set = new Set(arr);
+  return [...set];
+}
+
+//=====================================
+console.log(UniqueElements(arr));
 
 //just for more information
 //Que1) How to convert array element into string
